d2be7d1cad9a28e44e53decaa81ed04e0b688b65
Prakash Thirunavukkarasu <pthirunavukkarasu@riverbed.com>
2017-10-26 21:56:02 -0400

    fixed path

--- nodejs/src/Trees/factory/test.js
+++ nodejs/src/Trees/factory/test.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 // file is included here:
-eval(fs.readFileSync('nodejs/src/Trees/factory/nodes.js') +" ");
-
+eval(fs.readFileSync('nodes.js') +" ");
+var value = Math.floor(Math.random() * 10);
 var treeHeight = 3;
-var tmpb = generateSampleBinaryTree(treeHeight);
+var tmpb = generateSampleBinaryTree(value);
 printBTree(tmpb);
